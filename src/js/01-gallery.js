2d735206cabfc1d2d1e2cda095b6208f3c222b24
DariaRosliakova <dasha.roslyakova98@gmail.com>
2022-11-12 17:36:09 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryEl = galleryItems.map(({preview, original, description}) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`);
+
+gallery.insertAdjacentHTML("beforeend", galleryEl.join(''));
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionSelector: 'img',
+    captionsData: "alt",
+    captionPosition: 'bottom',
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const data = { 'email': '', 'message' : ''};
+
+
+form.addEventListener('input', throttle(onSetData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+onGetData() 
+function onSetData() {    
+    data.email = input.value;
+    data.message = textarea.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data))
+}
+
+
+function onGetData() {
+    const parseData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (parseData) {
+        input.value = parseData.email || ''
+        textarea.value = parseData.message || ''
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(data);
+    e.target.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+}
 
